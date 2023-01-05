36be57ab1054a821472adf13d7038ea160a79ae6
GGumBug <alsrl7538@gmail.com>
2023-01-05 17:42:25 +0900

    quizImage, 이벤트 시스템 생성 추가

--- Assets/Script/Scene/QuizScene.cs
+++ Assets/Script/Scene/QuizScene.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class QuizScene : MonoBehaviour
 {
     private void Start() {
+        UIManager.GetInstance().OpenUI("quizImage");
+        UIManager.GetInstance().SetEventSystem();
         QuizManager.GetInstance().CurrentQuizStart();
     }
 }
