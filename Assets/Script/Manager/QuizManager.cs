ec1b97cd602970ef440c802d5818a092cc1280c7
GGumBug <alsrl7538@gmail.com>
2023-01-05 17:42:43 +0900

    영상에 맞춰 사진 생성 로직 추가

--- Assets/Script/Manager/QuizManager.cs
+++ Assets/Script/Manager/QuizManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.UI;
 
 public class QuizManager : MonoBehaviour
 {
@@ -29,12 +30,27 @@ public class QuizManager : MonoBehaviour
     string curReliceName;
 
     VideoPlayer quizVideo;
+    Image quizImage;
 
     public void CurrentQuizStart()
     {
         GameObject quizVideogo = GameObject.FindGameObjectWithTag("VideoPlayer");
         quizVideo = quizVideogo.GetComponent<VideoPlayer>();
+        GetImage();
 
         quizVideo.clip = Resources.Load<VideoClip>($"Videos/"+quizVideoList[curQuizNumber]);
+
+        switch (curQuizNumber)
+        {
+            case 0:
+                quizImage.sprite = Resources.Load<Sprite>($"Images/"+quizVideoList[curQuizNumber]);
+                break;
+        }
+    }
+
+    public void GetImage()
+    {
+        GameObject quizImagego = UIManager.GetInstance().GetUI("quizImage");
+        quizImage = quizImagego.GetComponentInChildren<Image>();
     }
 }
