522c622b5dcf11ff65fb378da82f73bda7e24137
GGumBug <alsrl7538@gmail.com>
2023-01-05 14:58:18 +0900

    쇼케이스 이름을 리스트로 변환 후 쇼케이스 종류에 따라 curQuizNumber 결정

--- Assets/Script/Object/ShowCase.cs
+++ Assets/Script/Object/ShowCase.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class ShowCase : MonoBehaviour
 {
+
     public void EnterQuizScene()
     {
+        var showCaseNameList = new List<string>(ObjectManager.GetInstance().objectList.Keys);
+
+        for (int i = 0; i < showCaseNameList.Count; i++)
+        {
+            if (gameObject.name == showCaseNameList[i])
+            {
+                QuizManager.GetInstance().curQuizNumber = i;
+            }
+        }
+
         ScenesManager.GetInstance().ChangeScene(Scene.Quiz);
     }
 }
