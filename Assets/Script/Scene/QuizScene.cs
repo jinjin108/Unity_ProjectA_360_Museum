5c9c0c901747f0528f5b8d6cf30eef7dcd4bf055
GGumBug <alsrl7538@gmail.com>
2023-01-06 11:06:50 +0900

    OpenUI 제거

--- Assets/Script/Scene/QuizScene.cs
+++ Assets/Script/Scene/QuizScene.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class QuizScene : MonoBehaviour
 {
     private void Start() {
-        UIManager.GetInstance().OpenUI("quizImage");
-        UIManager.GetInstance().SetEventSystem();
         QuizManager.GetInstance().CurrentQuizStart();
     }
 }
