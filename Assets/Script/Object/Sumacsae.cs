0631cf6e55648c83cb853e10ab5b1c92d023b61b
GGumBug <alsrl7538@gmail.com>
2023-01-09 18:31:25 +0900

    수막새 스크립트 추가

---
+++ Assets/Script/Object/Sumacsae.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sumacsae : MonoBehaviour
+{
+    public string name;
+    public bool isClear;
+
+    public Sumacsae(string name)
+    {
+        this.name = name;
+        this.isClear = false;
+    }
+}
