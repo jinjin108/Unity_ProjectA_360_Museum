45f23e1fffb2617132adcad5c0a05e8f93eb432d
GGumBug <alsrl7538@gmail.com>
2023-01-10 17:42:42 +0900

    UI엔딩씬 스크립트 추가

---
+++ Assets/Script/UI/UIEndingScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIEndingScene : MonoBehaviour
+{
+    public Button btnSkipCredit;
+
+    private void Awake()
+    {
+        //btnSkipCredit = gameObject.GetComponentInChildren<Button>();
+    }
+}
