128f016fc991dbd1c7e12bda03328ceada507ce7
GGumBug <alsrl7538@gmail.com>
2023-01-10 12:07:32 +0900

    isDone  변수 추가 클리어 한 퀘스트 들어가지 않게 수정

--- Assets/Script/Object/ShowCase.cs
+++ Assets/Script/Object/ShowCase.cs
@@ -1,12 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ShowCase : MonoBehaviour
 {
+    public bool isDone;
+    XRGrabInteractable xrG;
+
+    private void Start()
+    {
+        xrG = GetComponent<XRGrabInteractable>();
+    }
+
+    private void Update()
+    {
+        if (isDone == true)
+        {
+            xrG.enabled = false;
+        }
+    }
 
     public void EnterQuizScene()
     {
+
+
         var showCaseNameList = new List<string>(ObjectManager.GetInstance().showCaseList.Keys);
 
         for (int i = 0; i < showCaseNameList.Count; i++)
